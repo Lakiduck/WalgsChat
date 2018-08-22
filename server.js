4f2bab45a9d1c5eef9d33af629c815147c53aa90
Lakiduck <lwalgama@hotmail.com>
2018-08-22 22:39:43 +1000

    Need to fix error so emit/broadcast work

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const express = require('express');
+var app = express();
+const path = require('path');
+
+app.use('/static', express.static(path.join(__dirname, 'static')));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+
+var server = app.listen(3000, function(req, res){
+  console.log('listening on port 3000!');
 });
 
-app.listen(3000, function(req, res){
-  console.log('listening on port 3000!');
+var io = require('socket.io').listen(server);
+
+io.on('connection', function(socket){
+  console.log('A user is connected');
+  /**socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });**/
 });
